1d628023dc5349540985b4f8f2975de9c94f5c3d
Glaucio Daniel <glauciodaniel@gmail.com>
2022-10-26 09:52:34 -0300

    Deploy

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/createUser.dto';
 
@@ -11,10 +18,16 @@ export class UsersController {
   create(@Body() req: CreateUserDTO) {
     return this.usersService.create();
   }
-  // listar todos
-
-  // listar um
-
+  // listar todos  localhost:3000/users
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+  // listar um  localhost:3000/users/1
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: number) {
+    return this.usersService.findOne(id);
+  }
   // atualizar
 
   // deletar
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,4 +5,12 @@ export class UsersService {
   async create(): Promise<string> {
     return 'Usuário criado com sucesso!';
   }
+
+  async findAll(): Promise<string> {
+    return 'Lista de usuários!';
+  }
+
+  async findOne(id: number): Promise<string> {
+    return `Usuário ${id}!`;
+  }
 }
